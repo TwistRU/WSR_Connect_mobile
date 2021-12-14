cd15abc591ccc24b4644e97aa21706e7f4283a59
s7rife <dimonchik22824@gmail.com>
2021-12-14 12:44:33 +1000

    Basic navigation architecture

--- app/src/main/java/ru/fefu/wsr_connect_mobile/NavBottomFragment.kt
+++ app/src/main/java/ru/fefu/wsr_connect_mobile/NavBottomFragment.kt
@@ -1,7 +1,20 @@
 package ru.fefu.wsr_connect_mobile
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.fefu.wsr_connect_mobile.databinding.FragmentNavBottomBinding
 
 
-class NavBottomFragment : BaseFragment<FragmentNavBottomBinding>(R.layout.fragment_nav_bottom){
+class NavBottomFragment : BaseFragment<FragmentNavBottomBinding>(R.layout.fragment_nav_bottom) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomNavView = binding.bottomNavView
+        val navController =
+            (childFragmentManager.findFragmentById(R.id.bottomNavContainer) as NavHostFragment).navController
+        bottomNavView.setupWithNavController(navController)
+    }
 }
